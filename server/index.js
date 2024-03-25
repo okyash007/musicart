9fec18b26727142576739198b91ea05a1953041d
yash <helloyashverma@gmail.com>
2024-03-25 23:09:26 +0530

    fixed another bug

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ connectDb().then(() => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://musicart-phi.vercel.app/"],
+    origin: ["http://localhost:5173", "https://musicart-phi.vercel.app"],
     credentials: true,
   })
 );
